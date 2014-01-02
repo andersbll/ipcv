47d1bb34e17494fa6ebc8fe9cac1d310fbbe8776
Anders Boesen Lindbo Larsen <abll@dtu.dk>
2014-01-02 10:45:20 +0100

    forgot to change __init__.py

--- ipcv/util/__init__.py
+++ ipcv/util/__init__.py
@@ -1,10 +1,13 @@
-from .image import stretch_intensity, imsave
+from .image import stretch_intensity, imsave, tile, patch, pad 
 from .interest_points import read_keypoints, write_keypoints, draw_keypoint, \
     extract_keypoint
 
 
 __all__ = ['stretch_intensity',
            'imsave',
+           'tile',
+           'patch',
+           'pad',
            'read_keypoints',
            'write_keypoints',
            'draw_keypoint',
