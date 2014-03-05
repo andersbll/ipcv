d2b1e8822ca73d8d84ab61228703b9a1d8d6a2a9
Anders Boesen Lindbo Larsen <abll@dtu.dk>
2014-03-05 14:51:43 +0100

    Added extract_patches function + edited image tile.

--- ipcv/util/__init__.py
+++ ipcv/util/__init__.py
@@ -1,12 +1,14 @@
-from .image import stretch_intensity, imsave, tile, patch, pad 
-from .interest_points import read_keypoints, write_keypoints, draw_keypoint, \
-    extract_keypoint
+from .image import (stretch_intensity, imsave, tile, patch, extract_patches,
+                    pad)
+from .interest_points import (read_keypoints, write_keypoints, draw_keypoint,
+                              extract_keypoint)
 
 
 __all__ = ['stretch_intensity',
            'imsave',
            'tile',
            'patch',
+           'extract_patches',
            'pad',
            'read_keypoints',
            'write_keypoints',
--- ipcv/util/image.py
+++ ipcv/util/image.py
@@ -27,17 +27,18 @@ def tile(imgs, aspect_ratio=1.0, tile_shape=None):
     '''
 
     # Prepare images
-    n_imgs = len(imgs)
-    img_shape = imgs[0].shape
+    imgs = np.array(imgs)
+    n_imgs = imgs.shape[0]
+    img_shape = imgs.shape[1:]
     assert len(img_shape) == 2 or len(img_shape) == 3
     if len(img_shape) == 2:
         # Add color dimension to greyscale images. This allows the rest of the
         # code to assume the image has a color dimension.
+        imgs = np.reshape(imgs, imgs.shape + (1,))
         img_shape = img_shape+(1,)
-        imgs = [img.reshape(img_shape) for img in imgs]
-    img_shape = np.array(img_shape)
 
     # Calculate grid shape
+    img_shape = np.array(img_shape)
     if tile_shape is None:
         img_aspect_ratio = img_shape[0] / float(img_shape[1])
         aspect_ratio *= img_aspect_ratio
@@ -54,7 +55,7 @@ def tile(imgs, aspect_ratio=1.0, tile_shape=None):
     tile_img_shape[:2] = (img_shape[:2] + spacing) * grid_shape[:2] - spacing
 
     # Assemble tile image
-    tile_img = np.zeros(tile_img_shape)
+    tile_img = np.ones(tile_img_shape)*np.min(imgs)
     for i in range(grid_shape[0]):
         for j in range(grid_shape[1]):
             img_idx = j + i*grid_shape[0]
@@ -98,6 +99,26 @@ def patch(img, pt, patch_size, padding='none'):
         else:
             raise ValueError('Invalid padding method')
     return patch
+
+
+def extract_patches(imgs, n_patches, patch_size, random_seed=None,
+                    filter_fun=None):
+    patches = np.empty((n_patches, patch_size, patch_size)+(imgs[0].shape[2:]))
+    i = 0
+    padding = patch_size//2+1
+    if random_seed is not None:
+        np.random.seed(random_seed)
+    while i < n_patches:
+        img_idx = np.random.randint(0, len(imgs))
+        img = imgs[img_idx]
+        y = np.random.randint(padding, img.shape[0]-padding)
+        x = np.random.randint(padding, img.shape[1]-padding)
+        patches[i, ...] = patch(img, (y, x), patch_size)
+        if filter_fun is not None:
+            if not filter_fun(patches[i, ...]):
+                continue
+        i += 1
+    return patches
 
 
 def pad(img, padding, fill_value=0):
